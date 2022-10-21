4d685012233960620b79e6ab9240dad71e2bd399
Ihor Kytsak <kitsak735@gmail.com>
2022-10-21 12:43:02 +0300

    skills page

--- src/containers/skills/index.js
+++ src/containers/skills/index.js
@@ -1,6 +1,63 @@
 import React from 'react';
+import { Line } from 'rc-progress';
 import { BsInfoCircleFill } from 'react-icons/bs';
 import PageHeaderContent from '../../components/pageHeaderContainer/portfolio';
+import { Animate, AnimateKeyframes } from 'react-simple-animate';
+import './styles.scss';
+
+const skillsData = [
+  {
+    label: 'FRONT END',
+    data: [
+      {
+        skillName: 'HTML',
+        percentage: '90',
+      },
+      {
+        skillName: 'CSS',
+        percentage: '50',
+      },
+      {
+        skillName: 'JAVASCRIPT',
+        percentage: '80',
+      },
+    ],
+  },
+  {
+    label: 'BACK END',
+    data: [
+      {
+        skillName: 'JAVA',
+        percentage: '70',
+      },
+      {
+        skillName: 'PYTHON',
+        percentage: '65',
+      },
+      {
+        skillName: 'KOTLIN',
+        percentage: '35',
+      },
+    ],
+  },
+  {
+    label: 'DATABASES',
+    data: [
+      {
+        skillName: 'SQL',
+        percentage: '70',
+      },
+      {
+        skillName: 'MONGO DB',
+        percentage: '65',
+      },
+      {
+        skillName: 'ORACLE',
+        percentage: '85',
+      },
+    ],
+  },
+];
 
 const Skills = () => {
   return (
@@ -9,6 +66,48 @@ const Skills = () => {
         headerText='My skills'
         icon={<BsInfoCircleFill size={40} />}
       />
+      <div className='skills__content-wrapper'>
+        {skillsData.map((item, i) => (
+          <div key={i} className='skills__content-wrapper__inner-content'>
+            <Animate
+              play
+              duration={1}
+              delay={0.3}
+              start={{
+                transform: 'translateX(-200px)',
+              }}
+              end={{
+                transform: 'translateX(0px)',
+              }}
+            >
+              <h3 className='skills__content-wrapper__inner-content__category-text'>
+                {item.label}
+              </h3>
+              <div className='skills__content-wrapper__inner-content__progressbar-container'>
+                {item.data.map((skillItem, j) => (
+                  <AnimateKeyframes
+                    play
+                    duration={1}
+                    keyframes={['opacity : 1', 'opacity : 0']}
+                    iterationCount='1'
+                  >
+                    <div className='progressbar-wrapper' key={j}>
+                      <p>{skillItem.skillName}</p>
+                      <Line
+                        percent={skillItem.percentage}
+                        strokeWidth='2'
+                        strokeColor='var(--yellow-theme-main-color)'
+                        trailWidth='2'
+                        strokeLinecap='square'
+                      />
+                    </div>
+                  </AnimateKeyframes>
+                ))}
+              </div>
+            </Animate>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
