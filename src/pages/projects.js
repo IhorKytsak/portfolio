83506f84526b3595f04dc39711728addf46b07cb
Ihor Kytsak <kitsak735@gmail.com>
2023-12-11 12:17:12 +0200

    adding articles page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import { motion } from 'framer-motion'
 
 import AnimatedText from '@/components/AnimatedText'
 import Layout from '@/components/Layout'
 import { GithubIcon } from '@/components/Icons'
 import project1 from '../../public/images/projects/crypto-screener-cover-image.jpg'
+
+const FramerImage = motion(Image)
 
 const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   return (
@@ -16,7 +19,13 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
         target='_blank'
         className='w-1/2 cursor-pointer overflow-hidden rounded-lg'
       >
-        <Image src={img} alt={title} className='w-full h-auto' />
+        <FramerImage
+          src={img}
+          alt={title}
+          className='w-full h-auto'
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
+        />
       </Link>
       <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
         <span className='text-primary font-medium text-xl'>{type}</span>
@@ -54,7 +63,13 @@ const Project = ({ type, title, img, link, github }) => {
         target='_blank'
         className='w-full cursor-pointer overflow-hidden rounded-lg'
       >
-        <Image src={img} alt={title} className='w-full h-auto' />
+        <FramerImage
+          src={img}
+          alt={title}
+          className='w-full h-auto'
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
+        />
       </Link>
       <div className='w-full flex flex-col items-start justify-between mt-4'>
         <span className='text-primary font-medium text-xl'>{type}</span>
