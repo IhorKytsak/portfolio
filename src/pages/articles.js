79911e5c2e0e5213df6343d861a9c8e216bf93e2
Ihor Kytsak <kitsak735@gmail.com>
2023-12-14 11:38:22 +0200

    dark mode part 2 + fix dm on refresh

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -58,17 +58,19 @@ const Article = ({ img, title, date, link }) => {
       initial={{ y: 200 }}
       whileInView={{ y: 0, transition: { duration: 0.5, ease: 'easeInOut' } }}
       viewport={{ once: true }}
-      className='relative w-full px-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4 '
+      className='relative w-full px-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4 dark:border-light dark:bg-dark dark:text-light'
     >
       <MovingImg title={title} img={img} link={link} />
-      <span className='text-primary font-semibold pl-4'>{date}</span>
+      <span className='text-primary dark:text-primaryDark font-semibold pl-4'>
+        {date}
+      </span>
     </motion.li>
   )
 }
 
 const FeaturedArticle = ({ img, title, time, summary, link }) => {
   return (
-    <li className='relative col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl'>
+    <li className='relative col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl dark:bg-dark dark:border-light'>
       <div className='absolute top-0 -right-3 -z-10 w-[101.5%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl' />
       <Link
         href={link}
@@ -89,7 +91,9 @@ const FeaturedArticle = ({ img, title, time, summary, link }) => {
         </h2>
       </Link>
       <p className='text-sm mb-2'>{summary}</p>
-      <span className='text-primary font-semibold'>{time}</span>
+      <span className='text-primary dark:text-primaryDark font-semibold'>
+        {time}
+      </span>
     </li>
   )
 }
@@ -101,7 +105,7 @@ const Articles = () => {
         <title>Articles Page</title>
         <meta name='description' content='any desc' />
       </Head>
-      <main className='w-full mb-16 flex flex-col items-center justify-center overflow-hidden'>
+      <main className='w-full mb-16 flex flex-col items-center justify-center overflow-hidden dark:text-light'>
         <Layout className='pt-16'>
           <AnimatedText text='Words Can Change The World!' className='mb-16' />
           <ul className='grid grid-cols-2 gap-16'>
