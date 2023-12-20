97770ba977835c4ce05177a7c66ac6b619499b89
Ihor Kytsak <kitsak735@gmail.com>
2023-12-20 11:30:39 +0200

    optimazing image components

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -73,6 +73,8 @@ const Project = ({ type, title, img, link, github }) => {
           className='w-full h-auto'
           whileHover={{ scale: 1.05 }}
           transition={{ duration: 0.2 }}
+          priority
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
         />
       </Link>
       <div className='w-full flex flex-col items-start justify-between mt-4'>
