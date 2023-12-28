39343197a85a217ddb6135ef970fec44f3c5630d
Ihor Kytsak <kitsak735@gmail.com>
2023-12-28 11:04:02 +0200

    added burger navigarion button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Link from 'next/link'
 import Logo from './Logo'
 import { useRouter } from 'next/router'
@@ -32,9 +33,35 @@ const CustomLink = ({ href, title, className = '' }) => {
 
 const NavBar = () => {
   const [mode, setMode] = useThemeSwitcher()
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+  }
 
   return (
     <header className='w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light'>
+      <button
+        className='flex-col justify-center items-center hidden lg:flex'
+        onClick={handleClick}
+      >
+        <span
+          className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm  ${
+            isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'
+          }`}
+        ></span>
+        <span
+          className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm my-0.5 ${
+            isOpen ? 'opacity-0' : 'opacity-100'
+          }`}
+        ></span>
+        <span
+          className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm  ${
+            isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'
+          }`}
+        ></span>
+      </button>
+
       <nav>
         <CustomLink href='/' title='Home' className='mr-4' />
         <CustomLink href='/about' title='About' className='mr-4' />
